27ca7609a60e37a858dbf7d6d49a096c81ff7d21
Michelle Vong <mva30@sfu.ca>
2022-07-18 00:35:47 -0700

    Set up basic back-end server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes/index');
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
