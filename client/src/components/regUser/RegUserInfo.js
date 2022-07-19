2d205b769125e7b748aa44887ee67fe6a9d331a2
Michelle Vong <mva30@sfu.ca>
2022-07-19 03:02:01 -0700

    Rename RegUserInfo function

--- client/src/components/regUser/RegUserInfo.js
+++ client/src/components/regUser/RegUserInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Dashboard() {
+export default function RegUserInfo() {
     return (
         <h1>This is where you can edit personal information.</h1>
     );
