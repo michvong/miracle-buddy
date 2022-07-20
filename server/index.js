afd1b9b61d7b404ec97e91ac41b5d3b938de35ea
Michelle Vong <mva30@sfu.ca>
2022-07-20 13:45:35 -0700

    Replace body-parser with express.json

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express();
 const cors = require("cors");
 const AppError = require("./utils/appError");
 const errorHandler = require("./utils/errorHandler");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const locationRouter = require("./routes/location-route");
 const serviceRouter = require("./routes/service-route")
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(cors());
 
