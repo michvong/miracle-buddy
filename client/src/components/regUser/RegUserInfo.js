f2f58d96386724716ffa6c36c8c9a9d5a7be54a9
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-22 13:49:58 -0700

    WIP: revert to functional component, ensure gui working

--- client/src/components/regUser/RegUserInfo.js
+++ client/src/components/regUser/RegUserInfo.js
@@ -8,61 +8,53 @@ import Dashboard from './Dashboard';
 
 
 
-class RegUserInfo extends Component {
+// class RegUserInfo extends Component {
+const RegUserInfo = (props) => {
 
-    // let [user_id, setUser] = useState([]);
+    let [user_id, setUser] = useState([]);
 
-    // useEffect(() => {
-    //     Axios.get(`http://localhost:3001/user/${user_id}`)
+    useEffect(() => {
+        Axios.get(`http://localhost:3001/user/${user_id}`)
+            .then((response) => {
+                setUser(response.data);
+            });
+    }, []);
+
+    // user_info()  {
+    //     const currentUser = {};
+    //     Axios.get(`http://localhost:3001/user/${this.state.user_id}`)
     //         .then((response) => {
-    //             setUser(response.data);
+    //             currentUser(response.data);
     //         });
-    // }, []);
-    constructor(props){
-        super(props)
-        const currentUser = {};
-        Axios.get(`http://localhost:3001/user/${this.state.user_id}`)
-            .then((response) => {
-                currentUser(response.data);
-            });
 
-        this.state = {
-            user_id: this.props.user_id,
-            currentUser: currentUser,
-        }
-    }
+    //     this.setState({
+    //         currentUser: currentUser,
+    //         user_id: this.state.user_id,
+    //     })
+    // }
 
-    user_info()  {
-        const currentUser = {};
-        Axios.get(`http://localhost:3001/user/${this.state.user_id}`)
-            .then((response) => {
-                currentUser(response.data);
-            });
-
-        this.setState({
-            currentUser: currentUser,
-            user_id: this.state.user_id,
-        })
-    }
-
-    render(){
-        return (
-            <Accordion>
-                <Accordion.Item eventKey='0'>
-                    <Accordion.Header>{this.state.user_id}</Accordion.Header>
-                    <Accordion.Body></Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey='1'>
-                    <Accordion.Header>{this.state.currentUser.name}</Accordion.Header>
-                    <Accordion.Body></Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey='2'>
-                    <Accordion.Header>{this.state.currentUser.name}</Accordion.Header>
-                    <Accordion.Body></Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-        );
-    }
+    return (
+        <Accordion>
+            <Accordion.Item eventKey='0'>
+                <Accordion.Header>NAME</Accordion.Header>
+                <Accordion.Body>
+                    tmp empty body
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey='1'>
+                <Accordion.Header>NAME</Accordion.Header>
+                <Accordion.Body>
+                    tmp empty body
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey='2'>
+                <Accordion.Header>NAME</Accordion.Header>
+                <Accordion.Body>
+                    tmp empty body
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+    );
 }
 
 export default RegUserInfo;
