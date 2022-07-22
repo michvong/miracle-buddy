e446cbdc8828b1085aa3b73d839185fafc944db2
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-21 23:21:54 -0700

    ADD/experiment: Add user route and test passing in url as param

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const controller = require('../controllers/user-controller');
 const router = express.Router();
 
-router.get('/user', controller.getUserInfo);
+router.get('/user/:user_id', controller.getUserInfo);
+// router.get('/user/', controller.getUserInfo);
 
 module.exports = router;
