3f6b34efc5e52ebc77ff41c61d548438311d4182
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-21 23:23:18 -0700

    WIP: Beginning of Edit user page.
    Need to: change to class, pass in props for proper setup and display,
    call controller using routes to edit user

--- client/src/components/regUser/RegUserInfo.js
+++ client/src/components/regUser/RegUserInfo.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Axios from 'axios';
 
 export default function RegUserInfo() {
+
+    let [user_id, setUser] = useState([]);
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3001/user/${user_id}`)
+            .then((response) => {
+                setUser(response.data);
+            });
+    }, []);
+
+
+
     return (
-        <h1>This is where you can edit personal information.</h1>
+        <Card style={{ width: '68rem' }}>
+            <Card.Body>
+                <Card.Title></Card.Title>
+            </Card.Body>
+        </Card>
     );
 }
