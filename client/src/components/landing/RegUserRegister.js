bb3aeea1d8e28771153917f739e53a63e1510d8f
Michelle Vong <mva30@sfu.ca>
2022-07-25 06:19:27 -0700

    Set-up input forms

--- client/src/components/landing/RegUserRegister.js
+++ client/src/components/landing/RegUserRegister.js
@@ -1,7 +1,92 @@
 import React from 'react';
+import {useEffect, useState} from "react";
+import Axios from "axios";
+import { Fragment } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { Button } from "@material-tailwind/react";
+import { useNavigate } from 'react-router-dom';
 
-export default function RegUserRegister() {
+export default function RegUserLogin() {
+    const [Users, setUsers] = useState([]);
+    const [selected, setSelected] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        Axios.get('http://localhost:3001/regusers')
+            .then((response) => { 
+                setUsers(response.data);
+                setSelected(response.data[0]);
+            });
+    }, []);
+
+    const handleUserSelect = (userName) => {
+        setSelected({ name: userName });
+    }
+
+    const handleLoginClick = () => {
+        navigate('/dashboard', { state: {name: selected.name, user_id: selected.user_id } });
+    }
+
+    const handleRegisterClick = () => {
+        navigate('/reguserregister');
+    }
+
+    const handleBackClick = () => {
+        navigate('/reguserlogin');
+    }
+
     return (
-        <h1>This is where you can register.</h1>
-    );
+        <>
+        <div className="bg-gradient-to-r from-yellow-100 via-blue-200 to-transparent w-full h-full animate-text">
+            <button className="p-2 hover:opacity-70" onClick={handleBackClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clipRule="evenodd" />
+                </svg>
+            </button>
+
+            <div className="flex justify-center pt-40">
+            </div>
+
+            <div className="flex justify-center pt-8 pb-4">
+                <h2 className="text-center text-3xl font-extrabold text-gray-900">Customer Registration</h2>
+            </div>
+
+            <h2 className="text-center text-2xl font-extrabold text-gray-900 pb-2">Please fill in the following <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-blue-300">details:</span></h2>
+
+            <div className="flex justify-center">
+                <div className="top-18 w-80 pb-40 max-w-sm mx-auto">
+                    <form>
+                        <div className="flex flex-wrap -mx-3 mb-4">
+                            <div className="w-full px-3">
+                                <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Name</label>
+                                <input id="name" type="text" className="form-input w-full text-gray-800" placeholder="Enter your name" required />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-wrap -mx-3 mb-4">
+                            <div className="w-full px-3">
+                                <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
+                                <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-wrap -mx-3 mb-4">
+                            <div className="w-full px-3">
+                                <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="language">Language</label>
+                                <input id="language" type="language" className="form-input w-full text-gray-800" placeholder="Enter your preferred language" required />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-wrap -mx-3 mt-6">
+                            <div className="w-full px-3 pb-4">
+                                <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Sign up</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </>
+    )
 }
