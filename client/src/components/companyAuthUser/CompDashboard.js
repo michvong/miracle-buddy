bb9ba85069416fc1ab303b23aea31d7fc88117f9
Michelle Vong <mva30@sfu.ca>
2022-07-25 05:34:55 -0700

    Add new icons for company dashboard

--- client/src/components/companyAuthUser/CompDashboard.js
+++ client/src/components/companyAuthUser/CompDashboard.js
@@ -74,10 +74,9 @@ export default function Dashboard() {
                     <button onClick={handleCompanyInfoClick}>
 
                         <CardHeader color="blue" className="relative h-56 flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" 
-                                className="h-20 w-20" viewBox="0 0 24 24" fill="currentColor">
-                                <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" 
-                                clipRule="evenodd" />
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20" fill="none" viewBox="0 0 24 24" 
+                                stroke="currentColor" stroke-width="2"> <path stroke-linecap="round" stroke-linejoin="round" 
+                                d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
                         </CardHeader>
 
@@ -128,11 +127,9 @@ export default function Dashboard() {
                 <Card className="mt-6 w-96">
                     <button onClick={handleWarehousesClick}>
                         <CardHeader color="blue" className="relative h-56 flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" 
-                                className="h-20 w-20" fill="none" 
-                                viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                <path strokeLinecap="round" strokeLinejoin="round" 
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20" fill="none" viewBox="0 0 24 24" 
+                                stroke="currentColor" stroke-width="2"> <path stroke-linecap="round" stroke-linejoin="round" 
+                                d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                             </svg>
                         </CardHeader>
                     </button>
