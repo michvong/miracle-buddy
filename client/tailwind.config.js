09f74db5899160c76f98a2e191d13f4f8615f605
Michelle Vong <mva30@sfu.ca>
2022-07-25 04:15:15 -0700

    Add text gradient to login landing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
 
   theme: {
     extend: {
+      'animation': {
+        'text':'text 5s ease infinite',
+      },
+      
+      'keyframes': {
+          'text': {
+              '0%, 100%': {
+                'background-size':'200% 200%',
+                  'background-position': 'left center'
+              },
+              '50%': {
+                'background-size':'200% 200%',
+                  'background-position': 'right center'
+              }
+          },
+      },
+
       backgroundImage: {
         'cityscape': "url('images/cityscape.png')",
       },
