b79525e62e903f909baa6ae40f09736524d50fa3
Michelle Vong <mva30@sfu.ca>
2022-07-24 23:10:50 -0700

    Complete UI for RegUserLogin

--- client/src/components/landing/RegUserLogin.js
+++ client/src/components/landing/RegUserLogin.js
@@ -1,6 +1,7 @@
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { Button } from "@material-tailwind/react";
 
 const people = [
   { name: 'Wade Cooper' },
@@ -11,63 +12,73 @@ const people = [
   { name: 'Hellen Schmidt' },
 ]
 
-export default function Example() {
+export default function RegUserLogin() {
   const [selected, setSelected] = useState(people[0])
 
   return (
+    <>
+    <div className="flex justify-center mt-20">
+    </div>
+
+    <div className="flex justify-center pt-20 pb-6">
+        <h2 className="text-center text-3xl font-extrabold text-gray-900">Sign in to your <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-blue-300">account</span></h2>
+    </div>
+
     <div className="flex justify-center">
-
-        <div className="fixed top-16 w-72">
-        <Listbox value={selected} onChange={setSelected}>
-            <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                <span className="block truncate">{selected.name}</span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <SelectorIcon
-                    className="h-5 w-5 text-gray-400"
-                    aria-hidden="true"/>
-                </span>
-            </Listbox.Button>
-            <Transition
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0">
-
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person, personIdx) => (
-                    <Listbox.Option
-                    key={personIdx}
-                    className={({ active }) =>
-                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                        }`
-                    }
-                    value={person}
-                    >
-                    {({ selected }) => (
-                        <>
-                        <span
-                            className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                            }`}
-                        >
-                            {person.name}
+        
+        <div className="top-16 w-80">
+            <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-1">
+                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                        <span className="block truncate">{selected.name}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <SelectorIcon
+                                className="h-5 w-5 text-gray-400"
+                                aria-hidden="true" />
                         </span>
-                        {selected ? (
-                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                            </span>
-                        ) : null}
-                        </>
-                    )}
-                    </Listbox.Option>
-                ))}
-                </Listbox.Options>
-            </Transition>
+                    </Listbox.Button>
+
+                    <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0">
+
+                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {people.map((person, personIdx) => (
+                                <Listbox.Option
+                                    key={personIdx}
+                                    className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'}`}
+                                    value={person}>
+
+                                    {({ selected }) => (
+                                        <>
+                                            <span
+                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                                                {person.name}
+                                            </span>
+                                            {selected ? (
+                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                </span>
+                                            ) : null}
+                                        </>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+            
+            
+            <div className="text-sm text-center m-3">If you are logging in as a company user, please click <a href="/companyauthlogin">here!</a></div>
+            <div className="flex justify-center">
+                <Button className="btn-blue m-0" size="sm" variant="gradient">Login</Button>
             </div>
-        </Listbox>
         </div>
     </div>
+    </>
+
   )
 }
