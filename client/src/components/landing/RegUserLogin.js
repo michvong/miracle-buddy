412b380b61681e99144569cbd49af9fc8d7d782a
Michelle Vong <mva30@sfu.ca>
2022-07-25 00:17:09 -0700

    Get drop-down menu to list users from database

--- client/src/components/landing/RegUserLogin.js
+++ client/src/components/landing/RegUserLogin.js
@@ -5,27 +5,21 @@ import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 import { Button } from "@material-tailwind/react";
 
-const people = [
-  { name: 'Wade Cooper' },
-  { name: 'Arlene Mccoy' },
-  { name: 'Devon Webb' },
-  { name: 'Tom Cook' },
-  { name: 'Tanya Fox' },
-  { name: 'Hellen Schmidt' },
-    // Axios.get('http://localhost:3001/users')
-]
-
 export default function RegUserLogin() {
     const [Users, setUsers] = useState([]);
+    const [selected, setSelected] = useState({});
 
-    useEffect(()=>{
-        Axios.get('http://localhost:3001/users')
+    useEffect(() => {
+        Axios.get('http://localhost:3001/regusers')
             .then((response) => { 
                 setUsers(response.data);
+                setSelected(response.data[0]);
             });
     }, []);
 
-    const [selected, setSelected] = useState(people[0])
+    const handleUserSelect = (userName) => {
+        setSelected({ name: userName });
+    }
 
     return (
         <>
@@ -39,7 +33,7 @@ export default function RegUserLogin() {
         <div className="flex justify-center">
             
             <div className="top-16 w-80">
-                <Listbox value={selected} onChange={setSelected}>
+                <Listbox value={selected} onChange={handleUserSelect}>
                     <div className="relative mt-1">
                         <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                             <span className="block truncate">{selected.name}</span>
@@ -57,17 +51,17 @@ export default function RegUserLogin() {
                             leaveTo="opacity-0">
 
                             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                {people.map((person, personIdx) => (
+                                {Users.map((val, personIdx) => (
                                     <Listbox.Option
                                         key={personIdx}
                                         className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'}`}
-                                        value={person}>
+                                        value={val.name}>
 
                                         {({ selected }) => (
                                             <>
                                                 <span
                                                     className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                                                    {person.name}
+                                                    {val.name}
                                                 </span>
                                                 {selected ? (
                                                     <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
@@ -91,6 +85,5 @@ export default function RegUserLogin() {
             </div>
         </div>
         </>
-
     )
 }
