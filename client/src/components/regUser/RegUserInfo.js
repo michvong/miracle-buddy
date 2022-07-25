12573cb6636d901805c5b26a139b0a0ee9cf322d
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-24 18:19:12 -0700

    ADD: add gui for user to input value for name change

--- client/src/components/regUser/RegUserInfo.js
+++ client/src/components/regUser/RegUserInfo.js
@@ -2,7 +2,7 @@ import React , { Component } from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
-import Accordion from 'react-bootstrap/Accordion';
+import {Accordion, Button} from 'react-bootstrap';
 import Axios from 'axios';
 import Dashboard from './Dashboard';
 
@@ -12,6 +12,9 @@ import Dashboard from './Dashboard';
 const RegUserInfo = (props) => {
 
     let [currentUser, setUser] = useState([]);
+    let [currentName, setName] = useState("");
+    let [currentLang, setLang] = useState("");
+    let [currentEmail, setEmail] = useState("");
 
     useEffect(() => {
         Axios.get(`http://localhost:3001/user/${props.user_id}`)
@@ -20,6 +23,12 @@ const RegUserInfo = (props) => {
             });
     }, []);
 
+    const handleSubmit= ((event) => {
+        event.preventDefault();
+
+
+        setName("");
+    });
 
     return (
         <Accordion>
@@ -29,7 +38,16 @@ const RegUserInfo = (props) => {
                         <Accordion.Item eventKey='0'>
                             <Accordion.Header>Current Name: {val.name}</Accordion.Header>
                             <Accordion.Body>
-                                tmp empty body
+                                <form onSubmit={handleSubmit}>
+                                    <label>
+                                        Enter new Name:
+                                        <input type="text"
+                                        value={currentName}
+                                        onChange={(e) => setName(e.target.value)}
+                                        />
+                                    </label>
+                                    <Button color= "blue" type="submit">Submit</Button>
+                                </form>
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey='1'>
