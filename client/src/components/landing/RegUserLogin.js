fecee997bdb63ab10de2a9b8703018ca315eaf29
Michelle Vong <mva30@sfu.ca>
2022-07-25 18:51:27 -0700

    Fix padding on login pages

--- client/src/components/landing/RegUserLogin.js
+++ client/src/components/landing/RegUserLogin.js
@@ -52,7 +52,7 @@ export default function RegUserLogin() {
 
             <div className="flex justify-center">
                 
-                <div className="top-16 w-80 pb-40">
+                <div className="top-16 w-80 pb-80">
                     <Listbox value={selected} onChange={handleUserSelect}>
                         <div className="relative mt-1">
                             <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
