ceaa8d07fbdd731307ca08a34d3354dca90a35d3
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-26 21:21:03 -0700

    ADD: able to change company info in db

--- server/routes/company-route.js
+++ server/routes/company-route.js
@@ -6,8 +6,8 @@ const router = express.Router();
 //acquires tuple info of company with id = company_id
 router.get('/company/:company_id', controllers.getCompInfo);
 //changes instance of attribute for company with id = company_id
-router.put('/company/edit-name/:company_id/:new_name', controllers.changeCompName);
-router.put('/company/edit-phone/:company_id/:new_phone', controllers.changeCompPhone);
-router.put('/company/edit-email/:company_id/:new_email', controllers.changeCompEmail);
+router.put('/company/edit-name/:company_id/:new_name', controllers.changeCompanyName);
+router.put('/company/edit-phone/:company_id/:new_phone', controllers.changeCompanyPhone);
+router.put('/company/edit-email/:company_id/:new_email', controllers.changeCompanyEmail);
 
 module.exports = router;
