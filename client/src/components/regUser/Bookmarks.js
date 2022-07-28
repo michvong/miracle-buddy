c14716bcc2fad80e91a62c83a0201b51d8c7574b
MarinEstrada <adrian.marin.estrada@gmail.com>
2022-07-27 19:17:34 -0700

    WIP: working on division for bookmarks

--- client/src/components/regUser/Bookmarks.js
+++ client/src/components/regUser/Bookmarks.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React , { Component } from 'react';
+import { useEffect, useState } from 'react';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import {Accordion, Button} from 'react-bootstrap';
+import Axios from 'axios';
+import Dashboard from './Dashboard';
 
 export default function Bookmarks() {
+
+    const info = useLocation();
+
+    let [currentUser, setUser] = useState([]);
+    let [testNumber, setNumber] = useState(0);
+    let [currentBookmarks, setBookmarks] = useState([]);
+
+    const fetch_user_id = () => {
+        try{
+            return info.state.user_id
+        } catch (e) {
+            Navigate('/reguserLogin')
+        }
+    }
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3001/bookmark-bookmarked-companies`)
+            .then((response) => {
+                if(response.status === 200) {
+                    setNumber(testNumber++);
+                }
+            })
+    })
+
+    // useEffect(() => {
+    //     Axios.get(`http://localhost:3001/user/${fetch_user_id()}`)
+    //         .then((response) => {
+    //             if(response.status === 200) {
+    //                 setUser(response.data);
+    //             }
+    //         });
+    // });
+
+
     return (
-        <h1>This is where you can view and edit bookmarks.</h1>
+        <Accordion>
+            {currentBookmarks.map}
+            <h1>{testNumber}</h1>
+        </Accordion>
+
     );
 }
--- server/controllers/bookmark-controller.js
+++ server/controllers/bookmark-controller.js
@@ -1,6 +1,7 @@
 const AppError = require("../utils/appError");
 const connection = require("../services/db");
 
+//added by Jacky
 exports.setBookmark = (req, res) => {
     const user_id = req.body.user_id;
     const company_id = req.body.company_id;
@@ -22,3 +23,28 @@ exports.deleteBookmark = (req, res) => {
             res.send(results);
         });
 };
+
+//added by Adri
+
+//get company id's of all bookmarked companies
+exports.getBookmarked = (req, res) => {
+    connection.query("SELECT DISTINCT x.company_id FROM Bookmarked AS x WHERE NOT EXISTS (SELECT * FROM required AS y WHERE NOT EXISTS (SELECT * FROM Bookmarked AS z WHERE (z.company_id = x.company_id) AND (z.user_id = y. user_id)))",
+        (err, results) => {
+            if(err) throw err;
+            res.send(results);
+        });
+};
+
+//get bookmarks of user with id = user_id
+exports.getAllBookmark = (req, res) => {
+    const user_id = req.params.user_id;
+
+    connection.query("SELECT * FROM Bookmarks WHERE user_id = ?",
+        [user_id],
+        (err, results) => {
+            if(err) throw err;
+            res.send(results);
+        });
+};
+
+
